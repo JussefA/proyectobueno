98fc2f0e65bf45d2548ed0c761deb34f0a6462b2
jussefA <jussef2009@gmail.com>
2019-04-08 22:09:20 -0700

    pantalla principal lista

--- Seccion_03_Recycler_Card_View-master/app/src/main/java/com/alejandrolora/seccion_03_recycler_card_view/activities/entrar.java
+++ Seccion_03_Recycler_Card_View-master/app/src/main/java/com/alejandrolora/seccion_03_recycler_card_view/activities/entrar.java
@@ -4,17 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 
 import com.alejandrolora.seccion_03_recycler_card_view.R;
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
 
 public class entrar extends AppCompatActivity {
 
     Button entrar;
+    private KenBurnsView kbv;
+    private boolean moving = true;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.entrar);
+
+        kbv = findViewById(R.id.kvb);
+
+        AccelerateDecelerateInterpolator adi = new AccelerateDecelerateInterpolator();
+        RandomTransitionGenerator generator = new RandomTransitionGenerator(2000,adi);
+        kbv.setTransitionGenerator(generator);
+        kbv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (moving){
+                    kbv.pause();
+                    moving = false;
+                } else{
+                   kbv.resume();
+                   moving = true;
+                }
+            }
+        });
 
         entrar = findViewById(R.id.entrarbtn);
 
